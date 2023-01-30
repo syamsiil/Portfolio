e8a210f5f6d3de48d915a1e52002e99b9d33f736
syamsiil <muhammadalisyamsi@gmail.com>
2023-01-30 11:34:13 +0700

    add contact

--- main.js
+++ main.js
@@ -8,9 +8,31 @@ function toggleDetail(e){
         $(target).html("Less Info"). addClass("active")
     }
 
+  
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
 
     $(detail).slideToggle()
 
 }    
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if (!$(email).val()) {
+        alert("Email is required")
+    } else if (!$(subject).val()) {
+        alert("Suject is required")
+    }else if (!$(message).val()) {
+        alert("Message is required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
+
